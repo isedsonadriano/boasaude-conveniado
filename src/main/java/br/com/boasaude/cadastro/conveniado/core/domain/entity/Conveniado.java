cb365a7abb064b1b3346af9cc73d2c78416c76ec
isedsonadriano <matracaweb@gmail.com>
2021-12-24 15:26:12 -0200

    Inclusão de novos campos no domínio da aplicação.

--- src/main/java/br/com/boasaude/cadastro/conveniado/core/domain/entity/Conveniado.java
+++ src/main/java/br/com/boasaude/cadastro/conveniado/core/domain/entity/Conveniado.java
@@ -1,5 +1,7 @@
 package br.com.boasaude.cadastro.conveniado.core.domain.entity;
 
+import br.com.boasaude.cadastro.conveniado.core.domain.enums.Status;
+import br.com.boasaude.cadastro.conveniado.core.domain.vo.TelefoneVO;
 import br.com.boasaude.cadastro.conveniado.core.domain.vo.TipoConveniado;
 
 public class Conveniado {
@@ -8,6 +10,8 @@ public class Conveniado {
 	private String nome;
 	private String cpf;
 	private TipoConveniado tipo;
+	private TelefoneVO telefone;
+	private Status status;
 
 	public Long getId() {
 		return id;
@@ -39,6 +43,22 @@ public class Conveniado {
 
 	public void setTipo(TipoConveniado tipo) {
 		this.tipo = tipo;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public TelefoneVO getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(TelefoneVO telefone) {
+		this.telefone = telefone;
 	}
 
 }
---
+++ src/main/java/br/com/boasaude/cadastro/conveniado/core/domain/enums/Status.java
@@ -0,0 +1,7 @@
+package br.com.boasaude.cadastro.conveniado.core.domain.enums;
+
+public enum Status {
+
+	ATIVO,
+	INATIVO
+}
